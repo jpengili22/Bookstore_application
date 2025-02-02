3f5af3055e030bcd7cdcde1d4bdd5c6caf5c0c85
sara <128810251+saraa2407@users.noreply.github.com>
2025-02-02 21:03:51 +0100

    Update FileHandler.java

--- Software-Test-Head/src/main/java/application/bookstore/auxiliaries/FileHandler.java
+++ Software-Test-Head/src/main/java/application/bookstore/auxiliaries/FileHandler.java
@@ -1,20 +1,50 @@
 package application.bookstore.auxiliaries;
 
-import application.bookstore.models.Author;
+
 import application.bookstore.models.BaseModel;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class FileHandler {
-    // write any model to the appropriate file
-    public static<T extends BaseModel> void overwriteCurrentListToFile(File file, ArrayList<T> data) throws IOException {
-        FileOutputStream fileOutputStream = new FileOutputStream(file, false);
-        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
-        for (T entity: data)
-            outputStream.writeObject(entity);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+
+  
+    public static <T extends BaseModel> void overwriteCurrentListToFile(File file, ArrayList<T> data) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file, false);
+             ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream)) {
+            for (T entity : data) {
+                outputStream.writeObject(entity);
+            }
+        }
+    }
+
+
+   
+    public static <T extends BaseModel> void overwriteCurrentListToFileAsync(File file, ArrayList<T> data) {
+        executor.submit(() -> {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file, false);
+                 ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream)) {
+                for (T entity : data) {
+                    outputStream.writeObject(entity);
+                }
+                System.out.println("Successfully wrote to file asynchronously: " + file.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    public static void shutdown() {
+        executor.shutdown();
     }
 }
