1d0f25b163b14e37a8b9db421a880835e581909c
sara <128810251+saraa2407@users.noreply.github.com>
2025-02-02 21:01:21 +0100

    Update Main.java

--- Software-Test-Head/src/main/java/application/bookstore/Main.java
+++ Software-Test-Head/src/main/java/application/bookstore/Main.java
@@ -14,12 +14,93 @@ import java.io.*;
 
 public class Main extends Application {
 
+    public static void main(String[] args)package application.bookstore;
+
+import application.bookstore.controllers.LoginController;
+import application.bookstore.models.Author;
+import application.bookstore.models.Role;
+import application.bookstore.models.User;
+import application.bookstore.views.LoginView;
+import application.bookstore.views.MainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main extends Application {
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2); 
+
     public static void main(String[] args) {
-//       seedData();
-//        createUser();
+
+        launch(args);
+        shutdownThreads();
+    }
+
+
+    private static void runCreateUserAsync() {
+        executor.submit(() -> {
+            User librarian = new User("manager", "Test2022", Role.MANAGER);
+            try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(User.FILE_PATH))) {
+                outputStream.writeObject(librarian);
+                System.out.println("Wrote user to the file users.ser successfully");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    private static void runSeedDataAsync() {
+        executor.submit(() -> {
+            User admin = new User("admin", "Test2022", Role.ADMIN);
+            User manager = new User("manager", "Test2022", Role.MANAGER);
+            User librarian = new User("librarian", "Test2022", Role.LIBRARIAN);
+            try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(User.FILE_PATH))) {
+                outputStream.writeObject(admin);
+                outputStream.writeObject(manager);
+                outputStream.writeObject(librarian);
+                System.out.println("Seeded users to the file users.ser successfully");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try (ObjectOutputStream authorOutputStream = new ObjectOutputStream(new FileOutputStream(Author.FILE_PATH))) {
+                authorOutputStream.writeObject(new Author("Test1", "Test1"));
+                authorOutputStream.writeObject(new Author("Test2", "Test2"));
+                authorOutputStream.writeObject(new Author("Test3", "Test3"));
+                System.out.println("Seeded authors to the file authors.dat successfully");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    
+    private static void shutdownThreads() {
+        executor.shutdown();
+        System.out.println("All threads shut down.");
+    }
+
+    @Override
+    public void start(Stage stage) {
+        LoginView loginView = new LoginView();
+        LoginController controller = new LoginController(loginView, new MainView(), stage);
+        Scene scene = new Scene(loginView.getView(), 320, 240);
+        stage.setTitle("Bookstore");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+ {
+
         launch(args);
     }
-//method to create user
+
     private static void createUser(){
 
         User librarian = new User("manager", "Test2022", Role.MANAGER);
@@ -34,7 +115,7 @@ public class Main extends Application {
     }
 
 
-    //method that was used to add first users
+  
     private static void seedData() {
         User admin = new User("admin", "Test2022", Role.ADMIN);
         User manager = new User("manager", "Test2022", Role.MANAGER);
@@ -49,14 +130,7 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(Author.FILE_PATH))) {
-//            outputStream.writeObject(new Author("Test1", "Test1"));
-//            outputStream.writeObject(new Author("Test2", "Test2"));
-//            outputStream.writeObject(new Author("Test3", "Test3"));
-//            System.out.println("Wrote authors to the file authors.dat successfully");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
     }
 
     @Override
